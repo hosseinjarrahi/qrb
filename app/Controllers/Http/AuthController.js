78439f99bd165ca0186bd5708c82c66dd1c304ab
hossein <hossein@localhost.localdomain>
2020-11-04 22:40:08 +0330

    add registring

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -1,17 +1,137 @@
 'use strict'
 
+const Kavenegar = require('kavenegar');
+const randomstring = require("randomstring");
+const moment = use('moment');
+const User = use('App/Models/User');
+
 class AuthController {
 
-  async login({request,auth}) {
-    const { phone, password } = request.all()
+  async login({request, auth}) {
+    const {phone, password} = request.all()
     return await auth.attempt(phone, password)
   }
 
-  async user({request,response,auth}) {
-    if (auth.user.id !== Number(params.id)) {
-      return response.json({message:'دسترسی به این بخش مجاز نمی باشد'});
+  async register({request, response}) {
+    let user = null
+    try {
+      user = await User.create({...request.only(['name', 'phone', 'password'])});
+      let code = await this.sendCode(request.post().phone);
+      user.active_code = code
+      user.save();
+    } catch (e) {
+      user = await User.query().where({phone: request.post().phone}).where({phone_verified: false}).first();
+      if (!user) {
+        if (e.errno === 1062) {
+          return response.status(400).json({message: 'این شماره تلفن قبلا ثبت شده است.'});
+        }
+      }
+      if (moment().subtract(2, 'minutes').isBefore(user.updated_at)) {
+        return response.status(400).json({message: 'پس از دو دقیقه دوباره امتحان کنید.'});
+      } else {
+        let code = await this.sendCode(request.post().phone);
+        user.fill({...request.only(['name', 'phone', 'password']),active_code:code});
+        await user.save();
+      }
     }
-    return auth.user
+
+    return response.status(200).json({message: 'کد تایید برای شما ارسال شد.', user});
+  }
+
+  async user({request, response, auth}) {
+    return {user: auth.getUser()}
+  }
+
+  async logout({request, response, auth}) {
+    await auth.logout()
+
+    return {message: 'با موفقیت خارج شدید'}
+  }
+
+  async sendCode(phone) {
+    let code = Math.floor(Math.random() * 9999) + 10000;
+    let message = "کد تایید همدرس: ";
+    message += code;
+
+    let api = Kavenegar.KavenegarApi({
+      apikey: '4778426C5839394D6562307A64635567314E6C6F426638614478732F6D56696C4B7336626E7857594F6E413D'
+    });
+
+    api.VerifyLookup({receptor: phone, token: code, template: 'verify'}, (response, status) => {
+      User.query().where('phone', phone).update({active_code: code});
+    });
+
+    return code
+  }
+
+  async resendCode({request, response}) {
+    let user = await User.query()
+      .where({phone: request.post().phone})
+      .where('updated_at', '<=', moment().subtract(2, 'minutes').format('YYYY-MM-DD HH:mm:ss'))
+      .first();
+
+    if (!user) {
+      return response.status(400).json({message: 'تنها هر دو دقیقه امکان ارسال پیامک وجود دارد'})
+    }
+
+    user.active_code = await this.sendCode(user.phone)
+    await user.save()
+
+    return response.status(200).json({message: 'با موفقیت ارسال شد.'});
+  }
+
+  async verification({request, response}) {
+    let user = await User.query()
+      .where({phone: request.post().phone})
+      .where({active_code: request.post().code})
+      .where('updated_at', '>', moment().subtract(2, 'minutes').format('YYYY-MM-DD HH:mm:ss'))
+      .first();
+
+    if (!user) {
+      return response.status(400).json({message: 'کد وارد شده اشتباه است.'})
+    }
+
+    if (!request.post().wantsReset) {
+      user.phone_verified = true
+      user.save()
+      return response.status(200).json({message: 'با موفقیت ثبت نام شدید.'});
+    }
+
+    let reset_token = await randomstring.generate(15);
+    user.fill({reset_token});
+    await user.save()
+
+    return response.status(200).json({message: 'تایید شد.', reset_token, user});
+  }
+
+  async resetPassword({request, response}) {
+    let user = await User.query().where({phone: request.post().phone}).first();
+
+    if (!user) {
+      return response.status(500).json({message: 'کاربری با این اطلاعات یافت نشد.'});
+    }
+
+    if (!user.isVerified()) {
+      return response.status(400).json({message: 'از بخش ثبت نام اقدام نمایید.'});
+    }
+
+    if (moment().subtract(2, 'minutes').isBefore(user.updated_at)) {
+      return response.status(400).json({message: 'پس از دو دقیقه دوباره امتحان کنید.'});
+    }
+
+    this.sendCode(request.post().phone)
+
+    return response.status(200).json({message: 'کد با موفقیت ارسال شد.'});
+  }
+
+  async changePassword({request, response}) {
+    let user = await User.query().where({phone: request.post().phone}).where({reset_token: request.post().reset_token}).first();
+
+    if (!user) {
+      return response.status(500).json({message: 'مشکلی رخ داده است.'});
+    }
+
+    return response.status(200).json({message: 'رمز شما با موفقیت تغییر یافت.'});
   }
 
 }
--- start/routes.js
+++ start/routes.js
@@ -2,11 +2,33 @@
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
+const User = use('App/Models/User')
+const Kavenegar = require('kavenegar');
+
+Route.get('/',() => {
+  let message = "کد تایید همدرس: ";
+
+  let api = Kavenegar.KavenegarApi({
+    apikey: '4778426C5839394D6562307A64635567314E6C6F426638614478732F6D56696C4B7336626E7857594F6E413D'
+  });
+
+  api.VerifyLookup({receptor: '09033144565', token: '123456', template: 'verify'}, (response, status) => {
+    // let user = User.query().where('phone', phone).update({active_code: code});
+  });
+
+  return 'as'
+})
 
 Route.get('/link/:link', 'user/HomeController.index')
 
 Route.post('login', 'AuthController.login').middleware('guest')
-Route.get('users/:id', 'AuthController.user').middleware('auth')
+Route.post('logout', 'AuthController.logout').middleware('auth')
+Route.post('register', 'AuthController.register').middleware('guest')
+Route.post('register/verification', 'AuthController.verification');
+Route.post('register/resend-code', 'AuthController.resendCode');
+Route.post('register/reset-password', 'AuthController.resetPassword');
+Route.post('register/change-password', 'AuthController.changePassword');
+Route.get('user', 'AuthController.user').middleware('auth')
 
 Route.group(() => {
   Route.resource('/product', 'coffe/ProductController').apiOnly();
